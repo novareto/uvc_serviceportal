fb328766ced40e8d0a4606a86261a86317c85db6
Souheil CHELFOUH <trollfot@gmail.com>
2020-07-01 21:17:10 +0200

    Added json schema read.

--- src/uvc_serviceportal/leikas/leika1.py
+++ src/uvc_serviceportal/leikas/leika1.py
@@ -2,6 +2,7 @@
 # # Copyright (c) 2007-2019 NovaReto GmbH
 # # cklinger@novareto.de
 
+from pathlib import Path
 from uvc_serviceportal.layout import template_endpoint
 from uvc_serviceportal import ROUTES
 from uvc_serviceportal.leikas import REG
@@ -11,18 +12,21 @@ from autoroutes import Routes
 from roughrider.routing.route import add_route as route
 
 
+PATH = Path(__file__)
+
+
 class Leika1(BaseFormularObject):
-    pass
 
-
-
+    def __init__(self, *args, **kwargs):
+        with PATH.with_suffix('.json').open() as fd:
+            kwargs['schema'] = fd.read()
+        super().__init__(*args, **kwargs)
 
 
 REG["leika1"] = Leika1(
     id="leika1",
     title=u"Leika Test",
     description="Leika Test Description",
-    schema=SCHEMA,
     output="<xml><uv></uv>",
     icon="bi bi-chevron-right",
 )

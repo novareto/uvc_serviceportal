7bdf6e9975bf8e4626974bb74c919b7bb97c39ed
Souheil Chelfouh <trollfot@gmail.com>
2020-07-07 17:15:20 +0200

    Added registry tests

--- tests/test_uv_serviceportal.py
+++ tests/test_uv_serviceportal.py
@@ -13,4 +13,22 @@ def test_content(app):
 def test_index(wsgiapp):
     resp = wsgiapp.get('/')
     assert(resp.status == '200 OK')
-    assert(resp.mustcontain(u"UVC-Service Portal") is None)
+    assert(resp.mustcontain("UVC-Service Portal") is None)
+
+
+def test_registry():
+    from uvc_serviceportal.leikas import BaseFormularObject
+    from uvc_serviceportal.leikas import REGISTRY
+
+    assert REGISTRY == {}
+    REGISTRY.load()
+    assert 'leika1' in REGISTRY
+
+    with pytest.raises(ValueError):
+        REGISTRY.register('test', object())
+
+    class Test(BaseFormularObject):
+        pass
+
+    test = Test('test', 'test', 'test', '{}', '', '')
+    REGISTRY.register('test', test)

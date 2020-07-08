309741091663bc9071a08d786c436d75a943247d
Christian Klinger <ck@novareto.de>
2020-07-08 15:23:38 +0200

    changes

--- src/uvc_serviceportal/static/csc/src/components/LandingPage.vue
+++ src/uvc_serviceportal/static/csc/src/components/LandingPage.vue
@@ -1,30 +1,36 @@
 <template>
   <div>
-    <div class="col-md-12">
-      <div class="form-group">
-        <label for="search_leika">Service Suchen</label>
-        <input v-model="filter.text" type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+    <div class="row">
+      <div class="col-md-12">
+        <div class="form-group">
+          <label for="search_leika">Service Suchen</label>
+          <input v-model="filter.text" type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" >
+        </div>
       </div>
+      <pre> {{filter}} </pre>
     </div>
-    <pre> {{filter}} </pre>
-    <div v-for="leika in leikas" :key="leika.id" >
-
+    <div class="row">
+      <div class="col-md-9">
+        <div v-for="leika in leikas" :key="leika.id" >
           <div class="card">
             <div class="card-body">
               <h5 class="card-title">{{leika.id}}</h5>
               <h6 class="card-subtitle mb-2 text-muted">{{leika.title}}</h6>
               <p class="card-text">{{leika.description}}</p>
-              <a :href="leika.id" class="btn btn-primary card-link">
-                  <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-plus" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                    <path fill-rule="evenodd" d="M8 3.5a.5.5 0 0 1 .5.5v4a.5.5 0 0 1-.5.5H4a.5.5 0 0 1 0-1h3.5V4a.5.5 0 0 1 .5-.5z"/>
-                    <path fill-rule="evenodd" d="M7.5 8a.5.5 0 0 1 .5-.5h4a.5.5 0 0 1 0 1H8.5V12a.5.5 0 0 1-1 0V8z"/>
-                  </svg>
-                  Anlegen
+              <a :href="leika.id" class="btn btn-primary card-link" >
+                <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-plus" fill="currentColor" xmlns="http://www.w3.org/2000/svg" >
+                  <path fill-rule="evenodd" d="M8 3.5a.5.5 0 0 1 .5.5v4a.5.5 0 0 1-.5.5H4a.5.5 0 0 1 0-1h3.5V4a.5.5 0 0 1 .5-.5z" />
+                  <path fill-rule="evenodd" d="M7.5 8a.5.5 0 0 1 .5-.5h4a.5.5 0 0 1 0 1H8.5V12a.5.5 0 0 1-1 0V8z" />
+                </svg>
+                Anlegen
               </a>
             </div>
           </div>
-
-
+        </div>
+      </div>
+      <div class="col-md-3">
+          PORTLET
+      </div>
     </div>
   </div>
 </template>
@@ -37,12 +43,12 @@ export default {
     }
   },
   data() {
-      return {
-          filter: {
-              text: ''
-          }
+    return {
+      filter: {
+        text: ""
       }
-  },
+    };
+  }
 };
 </script>
 

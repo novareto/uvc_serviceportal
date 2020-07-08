1ec7faae6e79402a5d379c2a595b5b452d803f57
Souheil Chelfouh <trollfot@gmail.com>
2020-07-08 16:22:41 +0200

    Added transaction

--- src/uvc_serviceportal/web.py
+++ src/uvc_serviceportal/web.py
@@ -1,9 +1,31 @@
+import transaction
 import horseman.meta
 import roughrider.routing.node
+
+from horseman.prototyping import Environ, StartResponse
 from roughrider.routing.route import add_route as route
+from uvc_serviceportal import ROUTES
+
 from .layout import template_endpoint
 from .leikas.components import REGISTRY
-from uvc_serviceportal import ROUTES
+
+
+class TestDataManager:
+
+    def tpc_vote(self, txn):
+        pass
+
+    def tpc_begin(self, txn):
+        print('Transaction commit begins.')
+
+    def commit(self, txn):
+        print('Transaction commiting !')
+
+    def tpc_finish(self, txn):
+        print('Transaction commit closing.')
+
+    def abort(self, txn):
+        print('Transaction aborted !')
 
 
 class Application(horseman.meta.SentryNode,
@@ -23,6 +45,11 @@ class Application(horseman.meta.SentryNode,
         exc_type, exc, traceback = exc_info
         self.logger.debug(exc)
 
+    def __call__(self, environ: Environ, start_response: StartResponse):
+        with transaction.manager as tr:
+            tr.join(TestDataManager())
+            yield from super().__call__(environ, start_response)
+
 
 @route(ROUTES, '/')
 @template_endpoint('index.pt')
@@ -32,3 +59,8 @@ def index(request):
         'leikas': REGISTRY,
         'leikas_json': REGISTRY.json()
     }
+
+
+@route(ROUTES, '/fail')
+def failing(request):
+    raise NotImplementedError('AAhhhh')
